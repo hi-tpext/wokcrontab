20c7300e1bb2c3fe7ebc72d4f8b1fe11afd96675
ichynul <ichynul@163.com>
2025-06-11 11:34:08 +0800

    修改版本号

--- common/Module.php
+++ common/Module.php
@@ -10,7 +10,7 @@ use tpext\common\ExtLoader;
  */
 class Module  extends baseModule
 {
-    protected $version = '1.0.15';
+    protected $version = '1.0.16';
 
     protected $name = 'wokman.crontab';
 
